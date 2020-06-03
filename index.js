c7a9b132077d1128f9c847907307f84a5a577337
Filipe <filipeccm@gmail.com>
2020-06-03 14:07:33 +0200

    form validation added

--- index.js
+++ index.js
@@ -6,39 +6,45 @@ function Book(name, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-};
-
-function addBookToLibrary() {
-  const bookForm = document.forms['book-form'];
-  const newBook = new Book(bookForm.elements['title'].value, 
-                           bookForm.elements['author'].value, 
-                           bookForm.elements['pages'].value,
-                           bookForm.elements['readstatus'].value);                
-  dbLibraryRef.push(newBook)
-  document.getElementById('popup').reset()
-  closeForm();
 }
 
-dbLibraryRef.on('value', snap => {
+function addBookToLibrary() {
+  const title = document.getElementById('title');
+  if (title.checkValidity()) {
+    const bookForm = document.forms['book-form'];
+    const newBook = new Book(
+      bookForm.elements['title'].value,
+      bookForm.elements['author'].value,
+      bookForm.elements['pages'].value,
+      bookForm.elements['readstatus'].value
+    );
+    dbLibraryRef.push(newBook);
+    document.getElementById('popup').reset();
+    closeForm();
+  } else {
+    return;
+  }
+}
+
+dbLibraryRef.on('value', (snap) => {
   const book = snap.val();
   if (book != null) {
     let keys = Object.keys(book);
     //clear the list so there won't be repeated cards
     const container = document.getElementById('container');
-    container.querySelectorAll('div').forEach(n => n.remove());
-    for(let i = keys.length - 1; i >= 0; i--) {
-  
+    container.querySelectorAll('div').forEach((n) => n.remove());
+    for (let i = keys.length - 1; i >= 0; i--) {
       const createDiv = document.createElement('div');
       const createH2 = document.createElement('h2');
       const authorP = document.createElement('p');
       const pagesP = document.createElement('p');
       const buttonDel = document.createElement('i');
       const buttonRead = document.createElement('button');
-      const createDiv2 = document.createElement('div')
+      const createDiv2 = document.createElement('div');
 
-      container.appendChild(createDiv)
-      createDiv.className = 'card'
-      createDiv.id = `${keys[i]}`
+      container.appendChild(createDiv);
+      createDiv.className = 'card';
+      createDiv.id = `${keys[i]}`;
 
       const thisDiv = document.getElementById(`${keys[i]}`);
       thisDiv.appendChild(createH2);
@@ -48,7 +54,7 @@ dbLibraryRef.on('value', snap => {
       thisDiv.appendChild(authorP);
       authorP.className = 'author';
       authorP.textContent = `${book[keys[i]].author}`;
-      
+
       thisDiv.appendChild(pagesP);
       pagesP.className = 'pages';
       pagesP.textContent = `${book[keys[i]].pages} pages`;
@@ -57,8 +63,8 @@ dbLibraryRef.on('value', snap => {
       createDiv2.className = 'btn-container';
 
       createDiv2.appendChild(buttonRead);
-      buttonRead.className = 'btn-read'
-      if(book[keys[i]].read === 'unread') {
+      buttonRead.className = 'btn-read';
+      if (book[keys[i]].read === 'unread') {
         buttonRead.textContent = 'Not Read Yet';
       } else {
         buttonRead.classList.add('done');
@@ -72,20 +78,27 @@ dbLibraryRef.on('value', snap => {
     }
   } else {
     const container = document.getElementById('container');
-    container.querySelectorAll('div').forEach(n => n.remove());
+    container.querySelectorAll('div').forEach((n) => n.remove());
   }
-  })
-  function deleteBook(event) {
-    dbLibraryRef.child(`${event.target.parentNode.parentNode.id}`).remove();
-  }
-  function changeRead(event) {
-    dbLibraryRef.child(`${event.target.parentNode.parentNode.id}/read`).once('value').then(snapshot => {
-      if(snapshot.val() === 'unread') {
-        dbLibraryRef.child(`${event.target.parentNode.parentNode.id}`).update({read: 'read'})
+});
+function deleteBook(event) {
+  dbLibraryRef.child(`${event.target.parentNode.parentNode.id}`).remove();
+}
+function changeRead(event) {
+  dbLibraryRef
+    .child(`${event.target.parentNode.parentNode.id}/read`)
+    .once('value')
+    .then((snapshot) => {
+      if (snapshot.val() === 'unread') {
+        dbLibraryRef
+          .child(`${event.target.parentNode.parentNode.id}`)
+          .update({ read: 'read' });
       } else {
-        dbLibraryRef.child(`${event.target.parentNode.parentNode.id}`).update({read: 'unread'})
+        dbLibraryRef
+          .child(`${event.target.parentNode.parentNode.id}`)
+          .update({ read: 'unread' });
       }
-  })
+    });
 }
 function openForm() {
   document.getElementsByClassName('form')[0].style.display = 'flex';
